cf328753b17d270668b1ce24764e07b0b7267f12
Egor Anikeev <egkeev@yandex-team.ru>
2022-11-12 19:18:29 +0300

    setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+function setup_xcframework {
+	cd $1
+	./gradlew buildReleaseXCFramework
+	cd ../
+}
+
+setup_xcframework KMMSharedCode
+setup_xcframework KMMSharedCodeCocoapods
+setup_xcframework KMMSharedCodeLower
+
+echo "Copying cocoapods xcframework"
+cp -r kmm_xcframework/Release/shared_cocoapods.xcframework SamplePackage/Support
+
+echo "Installing cocoapods to poject"
+cd SamplePackage/Example/Demo
+pod install
+cd ../../../
